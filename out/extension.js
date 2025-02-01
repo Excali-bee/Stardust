5f30d11879d257f0f3698b5190c74f1a3bae4eb7
Vitor Fazoli <86382456+Vitor-Fazoli@users.noreply.github.com>
2025-02-01 19:01:57 -0300

    Delete out directory

--- out/extension.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.activate = activate;
-exports.deactivate = deactivate;
-function activate(context) {
-    console.log('Thanks for using Stardust 🌟');
-}
-function deactivate() { }
-//# sourceMappingURL=extension.js.map
